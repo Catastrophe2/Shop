a4d905813d25f343e7c7044bc84a34b4e9784e7b
Yedidya <77005618+Catastrophe2@users.noreply.github.com>
2021-02-22 22:37:37 +0200

    Add files via upload

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Product from "../Product/Product.js";
 
-const Products = ({products, category}) => {
+const Products = ({products, category, range}) => {
     let filtered = [];
+    let rangeFiltered = products.filter((item) => {if(item.price>=range[0] && item.price <= range[1])return item})
     if (category === "all"){
-        filtered = products;
+        filtered = rangeFiltered;
     } else {
-        filtered = products.filter((item)=>{if(item.category === category)return item})
+        filtered = rangeFiltered.filter((item)=>{if(item.category === category)return item})
     }
-    
-    return filtered.map(({ id, image, title, price }) => (
-        <Product key={id} img={image} title={title} price={price} />
-    ));
+    if (rangeFiltered.length === 0){return <div>Sorry, nothing to show</div>} 
+    else{
+        return filtered.map(({ id, image, title, price }) => (
+            <Product key={id} img={image} title={title} price={price} idi = {id} />
+        ));
+    }
+
+
+
 }
 export default Products;
